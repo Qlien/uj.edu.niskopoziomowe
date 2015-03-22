d6760125f622fd0eba2cd1de3de5ac657b2817a2
Artem Shoobovych <shybovycha@gmail.com>
2015-03-22 15:06:22 +0100

    Fixed reading and converting num1

--- task3/task3.asm
+++ task3/task3.asm
@@ -60,13 +60,69 @@ str1_to_int_loop:
     mov rbx, [str1_len]
     dec rbx
 
-    dump_regs 11
+    cmp rcx, rbx
+    jge read_str2 ; if i >= str1_len - 1 then stop converting str1 to int
+
+    inc rcx ; i++
+    jmp str1_to_int_loop
+
+; -------------------------------------------------------------------------------
+
+read_str2:
+
+    mov rax, enter_num_msg
+    call print_string
+
+    xor rcx, rcx ; RCX = 0
+
+read_str2_loop:
+
+    xor rax, rax
+    call read_char
+
+    cmp al, 0ah
+    je str2_to_int ; if char == '\n' then stop reading str1
+
+    sub rax, '0' ; atoi
+
+    mov [tmp_buf2 + rcx], rax
+
+    cmp rcx, 5
+    jge str2_to_int ; if RCX >= 5 then stop reading str1
+
+    inc rcx
+    mov [str2_len], rcx ; str1_len = RCX
+    jmp read_str2_loop
+
+str2_to_int:
+
+    xor rax, rax ; RAX = 0
+    ; mov ch, [str1_len] ; RCX = str1_len
+    ; dec rcx ; RCX--
+    xor rcx, rcx ; i = 0
+
+str2_to_int_loop:
+
+    xor rbx, rbx
+    mov bl, [tmp_buf2 + rcx] ; RBX = str1[i]
+
+    mov rdx, 10 ; RDX = 10
+    mul rdx ; RAX = RAX * RDX
+
+    add rax, rbx ; RAX += digit
+    mov [num2], rax ; num1 = RAX
+
+    xor rdx, rdx
+    xor rbx, rbx
+
+    mov rbx, [str2_len]
+    dec rbx
 
     cmp rcx, rbx
     jge add_numbers ; if i >= str1_len - 1 then stop converting str1 to int
 
     inc rcx ; i++
-    jmp str1_to_int_loop
+    jmp str2_to_int_loop
 
 add_numbers:
 
