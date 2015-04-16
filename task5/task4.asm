264f7be25e780068739f735e502f10616137f28d
Artem Shoobovych <shybovycha@gmail.com>
2015-04-16 23:42:32 +0200

    Done lab 5 and 6

--- task5/task4.asm
+++ task5/task4.asm
@@ -1,4 +1,4 @@
-BITS 32
+BITS 64
 
 section .text
 
@@ -6,18 +6,26 @@ global minmax
 
 minmax:
 
-    push ebp
-    mov ebp, esp
+    push rbp
+    mov rbp, rsp
 
-    %define n dword [ebp + 12]
+    mov [n], edi ; first argv
 
     ; tu zaczyna się magia
 
+get_args:
+
+    mov [a], esi
+    mov [a + 1*4], edx
+    mov [a + 2*4], ecx
+    mov [a + 3*4], r8d
+    mov [a + 4*4], r9d
+
 find_min:
 
-    mov ecx, n ; i = n
+    mov ecx, [n] ; i = n
 
-    mov eax, [ebp + 12 + ecx * 4] ; eax = argv[n - 1] -> min
+    mov eax, [a - 4 + ecx * 4] ; eax = argv[n - 1] -> min
 
 find_min_loop:
 
@@ -26,7 +34,7 @@ find_min_loop:
     cmp ecx, 0
     jle find_max ; if i < 0 then break
 
-    mov ebx, [ebp + 12 + ecx * 4] ; ebx = a[i]
+    mov ebx, [a + ecx * 4] ; ebx = a[i]
 
     cmp ebx, eax
     jge find_min_loop ; if a[i] >= min then continue
@@ -38,9 +46,9 @@ find_min_loop:
 
 find_max:
 
-    mov ecx, n ; i = n
+    mov ecx, [n] ; i = n
 
-    mov eax, [ebp + 12 + ecx * 4] ; eax = argv[n - 1] -> min
+    mov eax, [a - 4 + ecx * 4] ; eax = argv[n - 1] -> min
 
 find_max_loop:
 
@@ -49,7 +57,7 @@ find_max_loop:
     cmp ecx, 0
     jle just_return ; if i < 0 then break
 
-    mov ebx, [ebp + 12 + ecx * 4] ; ebx = a[i]
+    mov ebx, [a + ecx * 4] ; ebx = a[i]
 
     cmp ebx, eax
     jle find_max_loop ; if a[i] <= max then continue
@@ -61,8 +69,10 @@ find_max_loop:
 
 just_return:
 
-    mov eax, max
-    mov edx, min
+    xor rax, rax
+    mov eax, [max]
+    shl rax, 4
+    or rax, [min]
 
     leave ; LEAVE = mov esp, ebp / pop ebp - konwencjonalne zakończenie procedury
     ret
@@ -71,3 +81,5 @@ section .data
 
     min dd 0
     max dd 0
+    n dd 0
+    a times 5 dd 0
--- task6/tasks.md
+++ task6/tasks.md
@@ -13,4 +13,29 @@ Do rejestru ECX (adres tekstu) jest przesyłany wskażnik na adres tekstu. Musi 
 
 ## task4
 
-    
+W funkcji `_Z5checkPKc` (dojść tam można pry pomocy `b _Z5checkPKc`) w dokładnie tej linijce
+
+    => 0x08048efb <+39>:    test   al,al
+
+trzeba ustalić rejestr `AL` równym **ZERO**:
+
+    set $al = 0
+    (gdb) p $ebp-0x4
+    $1 = (void *) 0xffffc464
+    set variable *0xffffc464=0x2ad
+
+Wynikiem jest żart =)
+
+    TOP SECRET
+    ----------
+    Puk puk.
+    - Kto tam?
+    (długa cisza)
+    - Java.
+
+    Puk puk.
+    - Kto tam?
+    - C++.
+
+    Puk puk.
+    - Assembler.
